55353625b146e51cd7724215d1fb0d3a84d9f593
David <dwrober@gmail.com>
2014-06-30 18:21:06 -0600

    simple silder example

--- src/com/example/colorpickerusingintent/FragmentMain.java
+++ src/com/example/colorpickerusingintent/FragmentMain.java
@@ -2,14 +2,22 @@ package com.example.colorpickerusingintent;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class FragmentMain extends Fragment {
+	
+	private SeekBar bender = null;
+	
+	private FrameLayout colorFrameBlend;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +45,37 @@ public class FragmentMain extends Fragment {
             	getColor2();
             }
         });
+		
+		bender = (SeekBar) getActivity().findViewById(R.id.benderSeeker);
+		
+		bender.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progressChanged = 0;
+ 
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                progressChanged = progress;
+                Log.i("seek_bar",Integer.toString(progressChanged));
+                setBenderColor(progressChanged);
+            }
+ 
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            	Log.i("seek_bar","onStartTrackingTouch");
+            }
+ 
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            	Log.i("seek_bar", "onStopTrackingTouch: " + Integer.toString(seekBar.getProgress()));
+            }
+        });
+	}
+	
+	private void setBenderColor(int progress) {
+		int red = progress;
+		int green = 0;
+		int blue = 0;
+		
+		colorFrameBlend = (FrameLayout) getActivity().findViewById(R.id.colorFrameBlend);
+		colorFrameBlend.setBackgroundColor(Color.rgb(red, green, blue));
+		
 	}
 	
 	private void getColor1() {
